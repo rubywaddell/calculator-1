81f1caf05743c393b185b1f81e758e2e67c8d509
Ru <rubywaddell@gmail.com>
2021-09-01 19:26:44 +0000

    update subtract function

--- arithmetic.py
+++ arithmetic.py
@@ -9,7 +9,7 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    return num2-num1
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
